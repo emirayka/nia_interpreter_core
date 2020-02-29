02d48d4ed1548066f1199a9cd8fc594dfd6a288b
Emirayka <emirayka@protonmail.com>
2020-02-29 12:20:31 +0300

    Little structure refactoring.

--- src/interpreter/stdlib/builtin_functions/mod.rs
+++ src/interpreter/stdlib/builtin_functions/mod.rs
@@ -1,14 +1,44 @@
 use crate::interpreter::error::Error;
 use crate::interpreter::interpreter::Interpreter;
+use crate::interpreter::function::builtin_function::BuiltinFunctionType;
+use crate::interpreter::lib::infect::infect_builtin_function;
 
-mod math;
-mod symbol;
+mod car;
+mod cdr;
+mod cons;
+mod div;
+mod flookup;
+mod gensym;
+mod intern;
 mod list;
+mod lookup;
+mod mul;
+mod rem;
+mod sub;
+mod sum;
 
 pub fn infect(interpreter: &mut Interpreter) -> Result<(), Error> {
-    math::infect(interpreter)?;
-    symbol::infect(interpreter)?;
-    list::infect(interpreter)?;
+    let pairs: Vec<(&str, BuiltinFunctionType)> = vec!(
+        ("car", car::car),
+        ("cdr", cdr::cdr),
+        ("cons", cons::cons),
+        ("list", list::list),
+
+        ("flookup", flookup::flookup),
+        ("gensym", gensym::gensym),
+        ("intern", intern::intern),
+        ("lookup", lookup::lookup),
+
+        ("/", div::div),
+        ("*", mul::mul),
+        ("%", rem::rem),
+        ("-", sub::sub),
+        ("+", sum::sum),
+    );
+
+    for (name, func) in pairs {
+        infect_builtin_function(interpreter, name, func)?;
+    }
 
     Ok(())
 }
