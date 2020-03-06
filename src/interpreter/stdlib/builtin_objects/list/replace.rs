3ca6ab2cbb6b01d3c93a040c288478be5e362cb2
Emirayka <emirayka@protonmail.com>
2020-03-06 18:56:57 +0300

    Added built-in function: "list:replace".

--- src/interpreter/stdlib/builtin_objects/list/replace.rs
+++ src/interpreter/stdlib/builtin_objects/list/replace.rs
@@ -3,15 +3,127 @@ use crate::interpreter::environment::environment_arena::EnvironmentId;
 use crate::interpreter::value::Value;
 use crate::interpreter::error::Error;
 
+use crate::interpreter::lib;
+
 pub fn stub(
     interpreter: &mut Interpreter,
     _environment: EnvironmentId,
     values: Vec<Value>
 ) -> Result<Value, Error> {
-    Ok(Value::Boolean(false))
+    if values.len() != 3 {
+        return interpreter.make_invalid_argument_count_error(
+            "Built-in function `list:replace' takes exactly three arguments."
+        ).into_result();
+    }
+
+    let mut values = values;
+
+    let index = lib::read_as_i64(
+        interpreter,
+        values.remove(0)
+    )? as usize;
+
+    let value = values.remove(0);
+
+    let mut values = lib::read_as_vector(
+        interpreter,
+        values.remove(0)
+    )?;
+
+    if let Some(value_ref) = values.get_mut(index) {
+        *value_ref = value;
+    } else {
+        return interpreter.make_invalid_argument_error(
+            "Built-in function `list:replace' takes a list that has enough items."
+        ).into_result();
+    }
+
+    // todo: probably change it because it's not optimal
+    Ok(interpreter.vec_to_list(values))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::interpreter::lib::assertion;
+
+    #[test]
+    fn replaces_element_in_list() {
+        let mut interpreter = Interpreter::new();
+
+        let pairs = vec!(
+            ("(list:replace 0 0 '(1 2 3 4))", "'(0 2 3 4)"),
+            ("(list:replace 1 0 '(1 2 3 4))", "'(1 0 3 4)"),
+            ("(list:replace 2 0 '(1 2 3 4))", "'(1 2 0 4)"),
+            ("(list:replace 3 0 '(1 2 3 4))", "'(1 2 3 0)"),
+        );
+
+        assertion::assert_results_are_equal(
+            &mut interpreter,
+            pairs
+        );
+    }
+
+    #[test]
+    fn returns_invalid_argument_error_when_list_has_zero_length() {
+        let mut interpreter = Interpreter::new();
+
+        let code_vector = vec!(
+            "(list:replace 1 1 '())",
+        );
+
+        assertion::assert_results_are_invalid_argument_errors(
+            &mut interpreter,
+            code_vector
+        );
+    }
+
+    #[test]
+    fn returns_invalid_argument_error_when_invalid_arguments_were_passed() {
+        let mut interpreter = Interpreter::new();
+
+        let code_vector = vec!(
+            "(list:replace 1.1 1 '(1 2 3))",
+            "(list:replace #t 1 '(1 2 3))",
+            "(list:replace #f 1 '(1 2 3))",
+            "(list:replace \"string\" 1 '(1 2 3))",
+            "(list:replace 'symbol 1 '(1 2 3))",
+            "(list:replace :keyword 1 '(1 2 3))",
+            "(list:replace '(1 2 3) 1 '(1 2 3))",
+            "(list:replace {} 1 '(1 2 3))",
+            "(list:replace #() 1 '(1 2 3))",
+
+            "(list:replace 0 1 1)",
+            "(list:replace 0 1 1.1)",
+            "(list:replace 0 1 #t)",
+            "(list:replace 0 1 #f)",
+            "(list:replace 0 1 \"string\")",
+            "(list:replace 0 1 'symbol)",
+            "(list:replace 0 1 :keyword)",
+            "(list:replace 0 1 {})",
+            "(list:replace 0 1 #())",
+        );
+
+        assertion::assert_results_are_invalid_argument_errors(
+            &mut interpreter,
+            code_vector
+        );
+    }
+
+    #[test]
+    fn returns_invalid_argument_count_error_when_incorrect_count_of_arguments_were_passed() {
+        let mut interpreter = Interpreter::new();
+
+        let code_vector = vec!(
+            "(list:replace)",
+            "(list:replace 1)",
+            "(list:replace 1 2)",
+            "(list:replace 1 2 3 4)",
+        );
+
+        assertion::assert_results_are_invalid_argument_count_errors(
+            &mut interpreter,
+            code_vector
+        );
+    }
 }
