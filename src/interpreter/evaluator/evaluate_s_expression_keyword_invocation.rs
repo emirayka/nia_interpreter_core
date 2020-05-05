e7fffa1975e806e3a292841f2c42e029b76fa110
Emirayka <emirayka@protonmail.com>
2020-05-06 01:45:47 +0300

    Extended keyword evaluation rule.

--- src/interpreter/evaluator/evaluate_s_expression_keyword_invocation.rs
+++ src/interpreter/evaluator/evaluate_s_expression_keyword_invocation.rs
@@ -1,11 +1,72 @@
 use crate::interpreter::evaluator::evaluate_value::evaluate_value;
 use crate::interpreter::evaluator::extract_arguments::extract_arguments;
-use crate::ConsId;
 use crate::EnvironmentId;
 use crate::Error;
 use crate::Interpreter;
 use crate::KeywordId;
 use crate::Value;
+use crate::{ConsId, SymbolId};
+
+fn evaluate_s_expression_keyword_get(
+    interpreter: &mut Interpreter,
+    environment_id: EnvironmentId,
+    property_symbol_id: SymbolId,
+    values: Vec<Value>,
+) -> Result<Value, Error> {
+    let mut values = values;
+    let object_value = values.remove(0);
+
+    let evaluated_argument =
+        evaluate_value(interpreter, environment_id, object_value)?;
+
+    let value = match evaluated_argument {
+        Value::Object(object_id) => {
+            interpreter.get_object_property(object_id, property_symbol_id)?
+        }
+        _ => {
+            return Error::generic_execution_error(
+                "Cannot get an item of not an object.",
+            )
+            .into();
+        }
+    };
+
+    match value {
+        Some(value) => Ok(value),
+        None => Ok(interpreter.intern_nil_symbol_value()),
+    }
+}
+
+fn evaluate_s_expression_keyword_set(
+    interpreter: &mut Interpreter,
+    environment_id: EnvironmentId,
+    property_symbol_id: SymbolId,
+    values: Vec<Value>,
+) -> Result<Value, Error> {
+    let mut values = values;
+
+    let object_value = values.remove(0);
+    let property_new_value = values.remove(0);
+
+    let evaluated_object_value =
+        evaluate_value(interpreter, environment_id, object_value)?;
+
+    let value = match evaluated_object_value {
+        Value::Object(object_id) => interpreter.set_object_property(
+            object_id,
+            property_symbol_id,
+            property_new_value,
+        )?,
+        _ => {
+            return Error::generic_execution_error(
+                "Cannot get an item of not an object.",
+            )
+            .into();
+        }
+    };
+
+    Ok(property_new_value)
+}
 
 pub fn evaluate_s_expression_keyword(
     interpreter: &mut Interpreter,
@@ -17,35 +78,27 @@ pub fn evaluate_s_expression_keyword(
         .get_keyword(keyword_id)
         .map(|keyword| keyword.get_name().clone())?;
 
-    let symbol_id = interpreter.intern_symbol_id(&keyword_name);
-
+    let property_symbol_id = interpreter.intern_symbol_id(&keyword_name);
     let mut arguments = extract_arguments(interpreter, cons_id)?;
 
-    if arguments.len() != 1 {
-        return Error::generic_execution_error(
-            "Invalid argument count in keyword s-expression.",
-        )
-        .into();
-    }
-
-    let argument = arguments.remove(0);
-
-    let evaluated_argument =
-        evaluate_value(interpreter, environment_id, argument)?;
-
-    match evaluated_argument {
-        Value::Object(object_id) => interpreter
-            .get_object_property(object_id, symbol_id)?
-            .ok_or_else(|| {
-                Error::generic_execution_error(
-                    "Object have not an item to yield.",
-                )
-            }),
+    match arguments.len() {
+        1 => evaluate_s_expression_keyword_get(
+            interpreter,
+            environment_id,
+            property_symbol_id,
+            arguments,
+        ),
+        2 => evaluate_s_expression_keyword_set(
+            interpreter,
+            environment_id,
+            property_symbol_id,
+            arguments,
+        ),
         _ => {
             return Error::generic_execution_error(
-                "Cannot get an item of not an object.",
+                "Invalid argument count in keyword s-expression.",
             )
             .into();
-        },
+        }
     }
 }
