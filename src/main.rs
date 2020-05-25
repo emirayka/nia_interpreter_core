0d2d1b34a0e2336182974cf9799301e39e626f2c
Emirayka <emirayka@protonmail.com>
2020-05-25 13:37:48 +0300

    Removed logging from main.

--- src/main.rs
+++ src/main.rs
@@ -17,32 +17,9 @@ use std::io::Write;
 // todo: binary plugins
 // todo: file system
 // todo: threading
+// todo: doitems loop that loops over object key value pairs
 
 fn main() -> std::io::Result<()> {
-    Builder::new()
-        .format(|buf, record| {
-            writeln!(
-                buf,
-                "{} [{}] - {}",
-                Local::now().format("%Y-%m-%dT%H:%M:%S"),
-                record.level(),
-                record.args()
-            )
-        })
-        .filter(None, LevelFilter::Debug)
-        .init();
-
-    println!(
-        "{}",
-        KeyChord::new(
-            vec!(Key::DeviceKey(DeviceKey::new(0, 58))),
-            Key::DeviceKey(DeviceKey::new(0, 2))
-        ) == KeyChord::new(
-            vec!(Key::LoneKey(LoneKey::new(58))),
-            Key::LoneKey(LoneKey::new(2))
-        )
-    );
-
     repl::run()?;
 
     Ok(())
